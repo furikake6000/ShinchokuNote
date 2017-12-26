b4a3af1ff28fb777fd846baf62481064b196e4bc
Furikake <ewave63@keio.jp>
2017-12-26 11:36:16 +0900

    トップページとaboutページ、形だけ作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #トップページ
+  root 'static_pages#home'
+  #固定ページ
+  get '/about', to: 'static_pages#about'
+
 end
