fc5eea17ddd02b1fa982a1f594c52702e6a32d18
Furikake <ewave63@keio.jp>
2017-12-27 17:37:40 +0900

    ユーザログイン関数の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
   validates :twitter_id, presence: true
+
+  #Omniauth Twitterを用いた認証
+  def User.twitter_login(auth)
+    User.find_or_create_by(twitter_id: auth[:uid])
+  end
+
+  #Cookieに保存された認証情報が正しいものであるか
+  #(Twitter IDとAPI User Token, API User Secretsの認証)
+  def User.check_cookie(twitter_id)
+    raise NotImplementedError.new("Function check_cookie() has not made yet.")
+  end
+
+  #現在ログインしているユーザを取得する
+  def User.current_user
+    raise NotImplementedError.new("Function current_user() has not made yet.")
+  end
 end
