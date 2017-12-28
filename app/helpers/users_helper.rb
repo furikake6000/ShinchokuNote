e13afe6e1426bda91a2328ecffac3d9cba756224
Furikake <ewave63@keio.jp>
2017-12-28 23:38:13 +0900

    ログイン関係の関数をUser.rbからusers_helper.rbに移設

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  #Omniauth Twitterを用いた認証
+  def twitter_login(auth)
+    #もし存在しなかったらユーザを作成する
+    User.find_or_create_by(twitter_id: auth[:uid])
+
+
+  end
+
+  #Cookieに保存された認証情報が正しいものであるか
+  #(Twitter IDとAPI User Token, API User Secretsの認証)
+  def check_cookie(twitter_id)
+    raise NotImplementedError.new("Function check_cookie() has not made yet.")
+  end
+
+  #現在ログインしているユーザを取得する
+  def current_user
+    raise NotImplementedError.new("Function current_user() has not made yet.")
+  end
 end
