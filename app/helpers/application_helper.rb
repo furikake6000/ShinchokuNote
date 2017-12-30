1af829292a92d6766b44a0e0a2ba803bd038113f
Furikake <ewave63@keio.jp>
2017-12-30 14:34:45 +0900

    共通鍵暗号の暗号化・復号化関数を作成（というより引用）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  #秘密鍵暗号方式（AES-256-CBC）でデータを暗号化する
+  #引用元（http://webos-goodies.jp/archives/encryption_in_ruby.html）
+  #暗号化
+  def encrypt_data(data, password, salt)
+    cipher = OpenSSL::Cipher::Cipher.new("AES-256-CBC")
+    cipher.encrypt
+    cipher.pkcs5_keyivgen(password, salt)
+    cipher.update(data) + cipher.final
+  end
+  #復号化
+  def decrypt_data(data, password, salt)
+    cipher = OpenSSL::Cipher::Cipher.new("AES-256-CBC")
+    cipher.decrypt
+    cipher.pkcs5_keyivgen(password, salt)
+    cipher.update(data) + cipher.final
+  end
+
 end
