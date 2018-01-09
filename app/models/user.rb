bcc9c55b5e998e35e112a6dcef426e8846fc50d9
Furikake <ewave63@keio.jp>
2018-01-09 22:36:59 +0900

    設計図など。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   validates :twitter_id, presence: true
 
+  #このユーザに付随しているユーザ（グループ）を取得
   def get_user_group_info
-    #このユーザに付随しているユーザ（グループ）を取得
-    raise NotImplementedError
+    #自分がマスタユーザでないとusergroupinfoは取得できない
+    raise IsNotMasterUserError if self.twitter_id != master_user_id
+
+    pass = master_user_token
+    
   end
 
   private
@@ -13,3 +17,5 @@ class User < ApplicationRecord
       OpenSSL::Digest::SHA256.digest(self.created_at.to_s)[0..7]
     end
 end
+
+class IsNotMasterUserError < RuntimeError; end
