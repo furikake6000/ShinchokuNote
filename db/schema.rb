308045b6f25d3c3128e4af78d8a102c6f9cce36c
Furikake <ewave63@keio.jp>
2018-01-12 16:45:34 +0900

    Userにpermission属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171231134202) do
+ActiveRecord::Schema.define(version: 20180112074239) do
 
   create_table "users", force: :cascade do |t|
     t.string "twitter_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20171231134202) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_group_info"
+    t.string "permission"
   end
 
 end
