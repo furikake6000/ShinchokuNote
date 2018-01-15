48167eafad68ff772a3ce1543f978f0f7a4e1859
Furikake <ewave63@keio.jp>
2018-01-15 20:11:03 +0900

    ユーザログインテスト...？

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get index" do
+    get :index
+    assert_redirected_to(root_path, message="Cant get index without logging in")
+
+    login_user(noritama, "noritama_token", "noritama_secret")
+    get :index
+    assert_redirected_to(root_path, message="Cant get index with logging in not as admin")
+
+    login_user(okaka, "okaka_token", "okaka_secret")
+    get :index
+    assert_response(:success, message="Get index with logging in as admin")
+  end
+
+
 end
