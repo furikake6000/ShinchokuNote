929d74e871dd1489650e3b603e609db3a037e33a
Furikake <ewave63@keio.jp>
2018-01-22 00:47:46 +0900

    Post作成

---
+++ db/migrate/20180121142431_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :text
+      t.string :type
+      t.float :order
+
+      t.timestamps
+
+      #For "TweetPost" < Post
+      t.string :twitter_id
+      t.string :media_urls
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121134948) do
+ActiveRecord::Schema.define(version: 20180121142431) do
 
   create_table "notes", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20180121134948) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "text"
+    t.string "type"
+    t.float "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "twitter_id"
+    t.string "media_urls"
   end
 
   create_table "users", force: :cascade do |t|
