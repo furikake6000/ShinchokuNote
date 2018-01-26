225061100faf454639d611ac730ed8006d2a26c0
Furikake <ewave63@keio.jp>
2018-01-27 01:16:41 +0900

    UserをよりRestfulに近づける、またNotesをRestfulに

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,21 @@ Rails.application.routes.draw do
   root 'users#home'
 
   #リソース：ユーザ
-  resources :users, :only => [:index, :new]
-  get '/u/:id', to: 'users#show'
+  resources :users, :only => [:new, :index, :show, :new]
+  get '/login', to: 'users#new'
+  get '/u/:id', to: 'users#show'  #リンク用短縮エイリアス
+  get '/auth/twitter/callback', to: 'users#login'
+  get '/switch', to: 'users#switchuser'
+  get '/logout', to: 'users#logout'
+
+  #リソース：ノート
+  resources :notes, :only => [:new, :show, :create, :edit, :update, :destroy]
+  get '/n/:id', to: 'notes#show'  #リンク用短縮エイリアス
 
   #固定ページ
   get '/about', to: 'static_pages#about'
   get '/manage', to: 'static_pages#manage'
 
   #ユーザ関連
-  get '/auth/twitter/callback', to: 'users#login'
-  get '/switch', to: 'users#switchuser'
-  get '/logout', to: 'users#logout'
 
 end
