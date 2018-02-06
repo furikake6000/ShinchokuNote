6aca10ce20d3d14126b565ad4ee133bf0333cbd7
furikake6000 <ewave63@keio.jp>
2018-02-07 00:32:33 +0900

    user.rbのrubocop指摘修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
 
   has_many :notes
 
-  #adminかどうかを返す
+  # adminかどうかを返す
   def admin?
-    self.permission == "admin"
+    permission == 'admin'
   end
 
-  #暗号化時のSaltとして使用するバイト列の生成
+  # 暗号化時のSaltとして使用するバイト列の生成
   def salt_8byte
-    #Created_atをSHA256でハッシュ化した最初の7バイト
-    OpenSSL::Digest::SHA256.digest(self.created_at.to_s)[0..7]
+    # Created_atをSHA256でハッシュ化した最初の7バイト
+    OpenSSL::Digest::SHA256.digest(created_at.to_s)[0..7]
   end
 end
