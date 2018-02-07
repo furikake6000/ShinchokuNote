e6de0bebc7a7fb6c6da3486064e49f2fa9bf0613
furikake6000 <ewave63@keio.jp>
2018-02-07 23:49:04 +0900

    各notetypeのコントローラを一部独立させる

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,20 @@ Rails.application.routes.draw do
   # リソース：ノート
   resources :users, shallow: true do
     # Shallowによりindex, new, createはuserから指定可能
-    resources :projects, controller: :notes, type: 'Project'
+    notetypes = [
+      { name: 'Project', sym: :projects },
+      { name: 'Idea', sym: :ideas },
+      { name: 'FinishedProject', sym: :finishedprojects }
+    ]
+    notetypes.each do |ntype|
+      # 一部イベントのみnotes_controllerで共通処理
+      resources ntype.sym,
+                controller: :notes,
+                type: ntype.sym,
+                only: %i[create update delete]
+      # 一部イベント以外は独自コントローラで処理
+      resources ntype.sym
+    end
   end
 
   # 固定ページ
