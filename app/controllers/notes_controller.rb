afd326484a9aee3d1d09747419272747221b70ff
furikake6000 <ewave63@keio.jp>
2018-02-10 14:41:31 +0900

    やはりnoteのURLはnote/idに変更

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,44 @@
 class NotesController < ApplicationController
-  before_action :note_user_collection, only: %i[destroy]
+  before_action :user_collection, only: %i[new create]
+  before_action :find_note, only: %i[show]
+  before_action :find_my_note, only: %i[edit update destroy]
 
   def index
     # Userのshowアクションと同じなのでリダイレクト
     redirect_to user_path(params[:user_id])
+  end
+
+  def new
+    @note = @user.notes.new
+  end
+
+  def show
+    # before_actionですでに@noteは取得済みなのでなにもしない
+  end
+
+  def create
+    @note = @user.notes.new(notes_params)
+    if @note.save
+      # 保存成功
+      redirect_to note_path(@note)
+    else
+      # やりなおし
+      render 'new'
+    end
+  end
+
+  def edit
+    # before_actionですでに@noteは取得済みなのでなにもしない
+  end
+
+  def update
+    if @note.update_attributes(notes_params)
+      # 保存成功
+      redirect_to note_path(@note)
+    else
+      # やりなおし
+      render 'edit'
+    end
   end
 
   def destroy
@@ -20,14 +55,22 @@ class NotesController < ApplicationController
   end
 
   # Note取得(自分のNote以外取得できない)
-  def note_user_collection
-    note_find
+  def find_my_note
+    find_note
     redirect_to root_path if current_user != @note.user
   end
 
   # Note取得
-  def note_find
+  def find_note
     @note = Note.find_by(id: params[:id])
     render_404 && return if @note.nil?
+  end
+
+  # Noteのパラメータを安全に取り出す
+  def notes_params
+    notetype = @note.type.downcase.to_sym unless @note.nil?
+    notetype ||= :note
+    print(notetype)
+    params.require(notetype).permit(:type, :name, :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,45 +3,17 @@ Rails.application.routes.draw do
   root 'users#home'
 
   # リソース：ユーザ
-  resources :users, only: %i[new index show]
+  # Shallowによりindex, new, createはuserから指定可能
+  resources :users, only: %i[new index show], shallow: true do
+    # リソース：ノート
+    resources :notes, shallow: true do
+      resources :posts
+    end
+  end
   get '/auth/twitter/callback', to: 'users#login'
   get '/switch', to: 'users#switchuser'
   get '/login', to: 'users#new'
   get '/logout', to: 'users#logout'
-
-  # リソース：ノート
-  # Shallowによりindex, new, createはuserから指定可能
-  resources :users, shallow: true do
-    # note typeごとにリソース宣言を行う
-    notetypes = [
-      { name: 'Project', sym: :projects },
-      { name: 'Idea', sym: :ideas },
-      { name: 'FinishedProject', sym: :finishedprojects }
-    ]
-    notetypes.each do |ntype|
-      # 一部イベントのみnotes_controllerで共通処理
-      resources ntype[:sym],
-                controller: :notes,
-                type: ntype[:name],
-                only: %i[index destroy]
-      # 一部イベント以外は独自コントローラで処理
-      resources ntype[:sym], shallow: true do
-        # post typeごとにリソース宣言を行う
-        posttypes = [
-          { name: 'TweetPost', sym: :tweetposts }
-        ]
-        posttypes.each do |ptype|
-          # 一部イベントのみnotes_controllerで共通処理
-          resources ptype[:sym],
-                    controller: :posts,
-                    type: ptype[:name],
-                    only: %i[index destroy]
-          # 一部イベント以外は独自コントローラで処理
-          resources ptype[:sym]
-        end
-      end
-    end
-  end
 
   # 固定ページ
   get '/about', to: 'static_pages#about'
