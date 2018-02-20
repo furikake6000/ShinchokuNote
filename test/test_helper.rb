00344d925193e9bdd0be7f4a3788a6692d586e11
furikake6000 <ewave63@keio.jp>
2018-02-21 02:09:26 +0900

    TestHelperのrubocop修正

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,24 +2,27 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml
+  # for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
 
-  #ApplicationHelperモジュールの書き換え
+  # ApplicationHelperモジュールの書き換え
   module ApplicationHelperFixes
-    #テスト用に再定義を行う
+    # テスト用に再定義を行う
 
-    #cookie取得
+    # cookie取得
     def getcookie(tag)
       cookies[tag.to_s]
     end
-    #cookie保存
+
+    # cookie保存
     def setcookie(tag, value)
       cookies[tag.to_s] = value
     end
-    #cookie削除
+
+    # cookie削除
     def deletecookie(tag)
       cookies.delete(tag.to_s)
     end
