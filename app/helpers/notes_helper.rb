281dde8404c05d58f824d41594529bdabed0b9cf
furikake6000 <ewave63@keio.jp>
2018-02-26 16:15:47 +0900

    ideasやfinishedprojects専用の置き場所を確保 相当するhelperメソッドを作成

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -3,4 +3,24 @@ module NotesHelper
   def current_users_note?(note)
     logged_in? && note.user == current_user
   end
+
+  # 特定ユーザのプロジェクトのみ取得
+  def projects_of(user)
+    user.notes.select { |p| p.type == 'Project' }
+  end
+
+  # 特定ユーザのリクエストボックスのみ取得
+  def request_boxes_of(user)
+    user.notes.select { |p| p.type == 'RequestBox' }
+  end
+
+  # 特定ユーザの構想中プロジェクトのみ取得
+  def ideas_of(user)
+    user.notes.select { |p| p.type == 'Idea' }
+  end
+
+  # 特定ユーザのプロジェクトのみ取得
+  def finished_projects_of(user)
+    user.notes.select { |p| p.type == 'FinishedProject' }
+  end
 end
