6ce1b1898a6f23c176be6f5b0537dda360f00531
furikake6000 <ewave63@keio.jp>
2018-03-10 12:46:55 +0900

    コメントのパーシャルを修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :from_user, class_name: 'User', foreign_key: 'from_user_id'
+  belongs_to :from_user,
+             class_name: 'User',
+             foreign_key: 'from_user_id',
+             optional: true
   belongs_to :to_note, class_name: 'Note', foreign_key: 'to_note_id'
   belongs_to :response_post,
              class_name: 'Post',
