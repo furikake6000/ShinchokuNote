b5fe06cc929753c5d89740394c73d6cdfa8a7a3c
furikake6000 <ewave63@keio.jp>
2018-04-11 17:32:20 +0900

    今日進捗を投稿したか否かの「posted_shinchoku_dodeska_today?」を作成

---
+++ app/helpers/shinchoku_dodeskas_helper.rb
@@ -0,0 +1,10 @@
+module ShinchokuDodeskasHelper
+  def posted_shinchoku_dodeska_today? (note, user)
+    ShinchokuDodeska.where(
+      "(from_user_id = ?) AND (to_note_id = ?) AND (created_at > ?)",
+      user.id,
+      note.id,
+      Time.now.beginning_of_day
+    )
+  end
+end
