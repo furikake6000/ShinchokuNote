ca597aaa5af0e1178f047462c395b83155626899
furikake6000 <ewave63@keio.jp>
2018-04-12 14:12:27 +0900

    helperのrubocop修正

--- app/helpers/shinchoku_dodeskas_helper.rb
+++ app/helpers/shinchoku_dodeskas_helper.rb
@@ -1,7 +1,7 @@
 module ShinchokuDodeskasHelper
-  def posted_shinchoku_dodeska_today? (note, user)
+  def posted_shinchoku_dodeska_today?(note, user)
     ShinchokuDodeska.where(
-      "(from_user_id = ?) AND (to_note_id = ?) AND (created_at > ?)",
+      '(from_user_id = ?) AND (to_note_id = ?) AND (created_at > ?)',
       user.id,
       note.id,
       Time.now.beginning_of_day
