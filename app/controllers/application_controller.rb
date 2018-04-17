736e2a92ea43527b969f3241ca4a971f9ec6924d
furikake6000 <ewave63@keio.jp>
2018-04-17 13:52:13 +0900

    エラーハンドリングを別ソースに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,14 @@ class ApplicationController < ActionController::Base
   include TwitterHelper
   include NotesHelper
   include CommentsHelper
+  include ErrorHandlingHelper
+
+  rescue_from Exception, with: :render_500
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+  rescue_from OAuth::Unauthorized, with: :render_401
 
   private
-
-  # 404ページ
-  def render_404
-    render file: Rails.root.join('public/404.html'), status: 404
-    true
-  end
-
-  # 403ページ
-  def render_403
-    render file: Rails.root.join('public/403.html'), status: 403
-    true
-  end
 
   def load_user(paramname)
     @user = User.find_by(screen_name: params[paramname].to_s)
---
+++ app/helpers/error_handling_helper.rb
@@ -0,0 +1,39 @@
+module ErrorHandlingHelper
+  # 参考: https://gist.github.com/itskingori/aae9a1905cec47f7fd52
+
+  # 404 Not Found
+  def render_404
+    respond_to do |format|
+      format.html{ render file: Rails.root.join('public/404.html'), :status => 404, layout: false }
+      format.xml{ render xml: 'Not Found', :status => 404 }
+      format.json{ render json: 'Not Found', :status => 404 }
+    end
+  end
+
+  # 403 Forbidden
+  def render_403
+    respond_to do |format|
+      format.html{ render file: Rails.root.join('public/403.html'), :status => 403, layout: false }
+      format.xml{ render xml: 'Forbidden', :status => 403 }
+      format.json{ render json: 'Forbidden', :status => 403 }
+    end
+  end
+
+  # 401 Unauthorized
+  def render_401
+    respond_to do |format|
+      format.html{ render file: Rails.root.join('public/401.html'), :status => 401, layout: false }
+      format.xml{ render xml: 'Unauthorized', :status => 401 }
+      format.json{ render json: 'Unauthorized', :status => 401 }
+    end
+  end
+
+  # 500 Internal Error
+  def render_500
+    respond_to do |format|
+      format.html{ render file: Rails.root.join('public/500.html'), :status => 500, layout: false }
+      format.xml{ render xml: 'Internal Error', :status => 500 }
+      format.json{ render json: 'Internal Error', :status => 500 }
+    end
+  end
+end
