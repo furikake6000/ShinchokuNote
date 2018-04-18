2623c4e762130270c1c0eafaa992002e0b9109e4
furikake6000 <ewave63@keio.jp>
2018-04-18 17:28:09 +0900

    rescue_fromがなくてもRailsがよしなにやってくれるし

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   include CommentsHelper
   include ErrorHandlingHelper
 
-  rescue_from Exception, with: :render_500
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from ActiveRecord::RecordNotFound,   with: :render_404
-  rescue_from OAuth::Unauthorized, with: :render_401
+  # rescue_from Exception, with: :render_500
+  # rescue_from ActionController::RoutingError, with: :render_404
+  # rescue_from ActiveRecord::RecordNotFound,   with: :render_404
+  # rescue_from OAuth::Unauthorized, with: :render_401
 
   private
 
