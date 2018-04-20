88f21f25e83f3650b0d48c9bd981973bd676ee91
furikake6000 <ewave63@keio.jp>
2018-04-20 10:30:11 +0900

    未ログイン時にウォッチリストに登録しようとするとログインを促すモーダルが生成される

--- app/helpers/shinchoku_dodeskas_helper.rb
+++ app/helpers/shinchoku_dodeskas_helper.rb
@@ -1,5 +1,6 @@
 module ShinchokuDodeskasHelper
   def posted_shinchoku_dodeska_today?(note, user)
+    return nil if user.nil?
     ShinchokuDodeska.where(
       '(from_user_id = ?) AND (to_note_id = ?) AND (created_at > ?)',
       user.id,
