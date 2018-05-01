8df6c0c6d70b2748ecd81d6cc51653bf4b51d07b
furikake <ewave63@keio.jp>
2018-05-01 23:27:57 +0900

    ？？？

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420042032) do
+ActiveRecord::Schema.define(version: 2018_04_20_042032) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
