f42e644f5138d0d0809221d79f5d2b644a1191a6
furikake6000 <ewave63@keio.jp>
2018-05-08 12:52:33 +0900

    image_form_virtualがアンダースコアじゃなくてハイフンになってたの統一感がないので修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w[
-  image-form-virtual.js
+  image_form_virtual.js
   copy_to_clipboard.js
   show_comments_tab.js
 ]
