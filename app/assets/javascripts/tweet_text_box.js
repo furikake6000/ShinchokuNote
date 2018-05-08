ff7e088837c2f7b6e8367366f7049c7a0517027f
furikake6000 <ewave63@keio.jp>
2018-05-08 14:04:17 +0900

    140文字以上を投稿できないように指定

--- app/assets/javascripts/tweet_text_box.js
+++ app/assets/javascripts/tweet_text_box.js
@@ -1,7 +1,16 @@
 $(document).on('turbolinks:load', function(){
     $tweettext_form = $("#tweettext_form");
     $tweettext_count = $("#tweettext_count");
+    $tweet_submit = $("#tweet_submit");
+
     $tweettext_form.keyup(function() {
-        $tweettext_count.text($tweettext_form.val().length);
+        let textlen = $tweettext_form.val().length;
+        $tweettext_count.text(textlen);
+
+        if(textlen > 140){
+            $tweet_submit.prop("disabled", true);
+        }else{
+            $tweet_submit.prop("disabled", false);
+        }
     });
 })
