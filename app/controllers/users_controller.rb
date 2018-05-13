334734ab72415b2bf45d93ae69b18009b018f96f
furikake <ewave63@keio.jp>
2018-05-13 22:27:18 +0900

    notificationsページにログアウト状態で入るとエラーになる問題を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :check_logged_in, only: %i[notifications notifications_checked]
   before_action -> { load_user_as_me_or_admin :id },
                 only: %i[edit update destroy]
   before_action -> { load_user_as_me_or_admin :user_id }, only: :leave
