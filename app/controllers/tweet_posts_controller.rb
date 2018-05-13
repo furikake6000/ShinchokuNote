be22321ea8aa36fe34ec18cd2664143fe5e5b4d6
furikake <ewave63@keio.jp>
2018-05-13 22:13:52 +0900

    tweet_postのcreate時に空の新規投稿を禁止

--- app/controllers/tweet_posts_controller.rb
+++ app/controllers/tweet_posts_controller.rb
@@ -4,6 +4,8 @@ class TweetPostsController < ApplicationController
   before_action -> { load_note_as_mine :note_id }, only: %i[create]
 
   def create
+    render_400 && return if params[:post][:twitter_id].nil? && params[:post][:text].empty?
+
     @post = tweetpost_from_params(@note)
     if @post.save
       # 保存成功
