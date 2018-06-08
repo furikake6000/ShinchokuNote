1e4ab867c750590a0dc78355ccd595efd8597c56
furikake6000 <ewave63@keio.jp>
2018-06-08 19:51:08 +0900

    ログアウト時も進捗どうですかを1日1回にできるように

--- app/helpers/shinchoku_dodeskas_helper.rb
+++ app/helpers/shinchoku_dodeskas_helper.rb
@@ -1,11 +1,21 @@
 module ShinchokuDodeskasHelper
   def posted_shinchoku_dodeska_today?(note, user)
-    return nil if user.nil?
-    ShinchokuDodeska.where(
-      '(from_user_id = ?) AND (to_note_id = ?) AND (created_at > ?)',
-      user.id,
-      note.id,
-      Time.now.beginning_of_day
-    ).empty?.!
+    unless user.nil?
+      # if user logged_in
+      ShinchokuDodeska.where(
+        '(from_user_id = ?) AND (to_note_id = ?) AND (created_at > ?)',
+        user.id,
+        note.id,
+        Time.now.beginning_of_day
+      ).empty?.!
+    else
+      # if user not logged_in
+      ShinchokuDodeska.where(
+        '(from_addr = ?) AND (to_note_id = ?) AND (created_at > ?)',
+        request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip,
+        note.id,
+        Time.now.beginning_of_day
+      ).empty?.!
+    end
   end
 end
