4d4944b72af51ff89416f87ff79f90102b591226
furikake6000 <ewave63@keio.jp>
2018-06-11 16:49:40 +0900

    トップページで読み込まれるポスト数を10から30に増加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
                 only: %i[edit update destroy]
   before_action -> { load_user_as_me_or_admin :user_id }, only: :leave
   before_action -> { load_user :id }, only: :show
-  before_action -> { load_newest_posts 10 }, only: :home
-  before_action -> { load_watching_posts 10 }, only: :home
+  before_action -> { load_newest_posts 30 }, only: :home
+  before_action -> { load_watching_posts 30 }, only: :home
   before_action :load_notifications, only: :notifications
   before_action :load_twitter_friends, only: %i[recommended_users]
 
