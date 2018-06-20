ff39e1f2a9a43ef1f62d7f4df4041561e02c4088
furikake6000 <ewave63@keio.jp>
2018-06-20 17:46:19 +0900

    ユーザのノート一覧を共有するボタンを作る

--- app/assets/javascripts/copy_to_clipboard.js
+++ app/assets/javascripts/copy_to_clipboard.js
@@ -4,6 +4,8 @@ function copyShareUrlToClipboard() {
 
     $("#share_icon").removeClass("fa-share-alt");
     $("#share_icon").addClass("fa-check");
+    $("#share_button").removeClass("btn-primary");
     $("#share_button").removeClass("btn-secondary");
+    $("#share_button").removeClass("btn-info");
     $("#share_button").addClass("btn-success");
 }
