12861d7cce249f9c724543d7982dfdc1b7a8f3d2
furikake <ewave63@keio.jp>
2018-06-24 01:32:32 +0900

    キーワードが指定されていない時の処理を追加

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def search
     @keyword = params[:search][:keyword]
+    
+    return if @keyword.empty?
+
+    @notes = Note.where("name LIKE ?", "%#{escape_like(@keyword)}%")
   end
 end
