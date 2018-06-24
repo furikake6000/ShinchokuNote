38249442a5bb5109a72d7f286a64d5be9d68f768
furikake <ewave63@keio.jp>
2018-06-24 19:45:13 +0900

    検索ページにpaginateやらorderやらをつけた

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,12 @@
 class SearchController < ApplicationController
   def search
     @keyword = params[:search][:keyword]
-    
+
     return if @keyword.empty?
 
-    @notes = Note.where("name LIKE ?", "%#{escape_like(@keyword)}%")
+    @notecount = Note.where('name LIKE ?', "%#{escape_like(@keyword)}%").count
+    @notes = Note.where('name LIKE ?', "%#{escape_like(@keyword)}%")
+                 .order('created_at DESC')
+                 .paginate(page: params[:page], per_page: 30)
   end
 end
