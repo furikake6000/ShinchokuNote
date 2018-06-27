c1c0a09a727ab6f48d0b82dab9a371cc9fc31410
furikake <ewave63@keio.jp>
2018-06-27 12:30:23 +0900

    Encrypted secretsからCredentialsへの移行

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter,Rails.application.secrets.twitter_api_key,Rails.application.secrets.twitter_api_secret
+  if Rails.env == 'production'
+    provider :twitter,
+             Rails.application.credentials.twitter[:key],
+             Rails.application.credentials.twitter[:secret]
+  else
+    provider :twitter,
+             Rails.application.credentials.twitter_dev[:key],
+             Rails.application.credentials.twitter_dev[:secret]
+  end
 end
