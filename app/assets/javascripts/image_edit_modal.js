7fe41b51704ed2aa43c0815781a454dc9b81fb55
furikake <ewave63@keio.jp>
2018-07-07 18:19:51 +0900

    画像編集モーダル、スマホのタッチ処理に対応

--- app/assets/javascripts/image_edit_modal.js
+++ app/assets/javascripts/image_edit_modal.js
@@ -27,7 +27,7 @@ $(document).on('turbolinks:load', function(){
     function getExpandRate(){
         // canvas.width()は実際の横幅
         // canvas[0].widthはcanvasの内部的解像度
-        return $canvas.width() / $canvas[0].width
+        return $canvas.width() / $canvas[0].width;
     }
 
     function getClickedCanvasPos(e){
@@ -38,14 +38,23 @@ $(document).on('turbolinks:load', function(){
         return new Point(mouse_pos_X, mouse_pos_Y);
     }
 
+    function getTouchedCanvasPos(e){
+        let rect = e.target.getBoundingClientRect();
+        let tpos = e.originalEvent.changedTouches[0];
+        let exp_rate = getExpandRate();
+        let touch_pos_X = Math.floor((tpos.clientX - rect.left) / exp_rate);
+        let touch_pos_Y = Math.floor((tpos.clientY - rect.top) / exp_rate);
+        return new Point(touch_pos_X, touch_pos_Y);
+    }
+
     $canvas_ui.mousedown(function(e){
         start_pos = getClickedCanvasPos(e);
         end_pos = null;
-    })
+    });
 
     $canvas_ui.mouseup(function(e){
         end_pos = getClickedCanvasPos(e);
-    })
+    });
 
     $canvas_ui.mousemove(function(e){
         if(start_pos && !end_pos){
@@ -54,6 +63,31 @@ $(document).on('turbolinks:load', function(){
             let rect_height = mouse_pos.Y() - start_pos.Y();
 
             ctx_ui.clearRect(0, 0, $canvas_ui[0].width, $canvas_ui[0].height);
+
+            ctx_ui.strokeRect(start_pos.X(), start_pos.Y(), rect_width, rect_height);
+        }
+    });
+
+    $canvas_ui.on("touchstart", function(e){
+        event.preventDefault();
+        start_pos = getTouchedCanvasPos(e);
+        end_pos = null;
+    });
+
+    $canvas_ui.on("touchend", function(e){
+        event.preventDefault();
+        end_pos = getTouchedCanvasPos(e);
+    });
+
+    $canvas_ui.on("touchmove", function(e){
+        event.preventDefault();
+        if(start_pos && !end_pos){
+            let touch_pos = getTouchedCanvasPos(e);
+            let rect_width = touch_pos.X() - start_pos.X();
+            let rect_height = touch_pos.Y() - start_pos.Y();
+
+            ctx_ui.clearRect(0, 0, $canvas_ui[0].width, $canvas_ui[0].height);
+
             ctx_ui.strokeRect(start_pos.X(), start_pos.Y(), rect_width, rect_height);
         }
     });
