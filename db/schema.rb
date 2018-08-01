d4ae0797765d6a35a1a7f3b2a41dd34a94431e94
furikake6000 <ewave63@keio.jp>
2018-08-01 22:10:49 +0900

    ActiveStorageの有効化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_052113) do
+ActiveRecord::Schema.define(version: 2018_08_01_111056) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "announces", force: :cascade do |t|
     t.string "text"
@@ -110,8 +131,8 @@ ActiveRecord::Schema.define(version: 2018_07_20_052113) do
     t.datetime "checked_notifications_at"
     t.binary "linked_users_info"
     t.datetime "saw_notifications_at"
-    t.boolean "comment_webpush_enabled", default: true
-    t.boolean "shinchoku_dodeska_webpush_enabled", default: true
+    t.boolean "comment_webpush_enabled"
+    t.boolean "shinchoku_dodeska_webpush_enabled"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["twitter_id"], name: "index_users_on_twitter_id", unique: true
   end
