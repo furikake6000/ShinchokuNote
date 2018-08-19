c2ef8b2518e34aceaa895fdb52f80b611fe552d9
furikake6000 <ewave63@keio.jp>
2018-08-19 11:45:33 +0900

    ページ読み込み時に自動で読み込むように

---
+++ app/assets/javascripts/newest_posts.js
@@ -0,0 +1,3 @@
+$(document).on('turbolinks:load', function(){
+    $("#newest_posts_load_button")[0].click();
+})
---
+++ app/assets/javascripts/watching_posts.js
@@ -0,0 +1,3 @@
+$(document).on('turbolinks:load', function(){
+    $("#watching_posts_load_button")[0].click();
+})
