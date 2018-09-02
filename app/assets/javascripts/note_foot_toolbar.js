028315fe3e23084da92bdc8955628c9519b32e43
furikake6000 <ewave63@keio.jp>
2018-09-03 02:27:00 +0900

    進捗どうですかタブが開く機構を追加 ついでに既存部分を「控えめなJS」に改修

--- app/assets/javascripts/note_foot_toolbar.js
+++ app/assets/javascripts/note_foot_toolbar.js
@@ -1,3 +1,19 @@
-function showCommentsTab(){
-    $('#commentslist_tab').tab('show');
-}
+$(document).on('turbolinks:load', function(){
+    var $shinchoku_dodeska_tab = $('#shinchoku_dodeska_tab');
+
+    $('.comments_tab_toggler').on('click', function() {
+        $('#commentslist_tab').tab('show');
+    });
+
+    $('.shinchoku_dodeska_tab_toggler').hover(function() {
+        $shinchoku_dodeska_tab.removeClass('d-none');
+    }, function() {
+        $shinchoku_dodeska_tab.addClass('d-none');
+    });
+
+    $shinchoku_dodeska_tab.hover(function() {
+        $shinchoku_dodeska_tab.removeClass('d-none');
+    }, function() {
+        $shinchoku_dodeska_tab.addClass('d-none');
+    });
+});
