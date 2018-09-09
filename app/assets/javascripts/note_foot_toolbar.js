afea11cb430d5f3ed30db594873537dfa4b1954e
furikake6000 <ewave63@keio.jp>
2018-09-09 23:52:23 +0900

    スマートフォン用のスタンプタブの仕様を作成

--- app/assets/javascripts/note_foot_toolbar.js
+++ app/assets/javascripts/note_foot_toolbar.js
@@ -5,7 +5,22 @@ $(document).on('turbolinks:load', function(){
         $('#commentslist_tab').tab('show');
     });
 
-    $('.shinchoku_dodeska_tab_toggler').hover(function() {
+    // スマートフォンなど用の挙動
+    // 長おしで出る右クリックメニューをなくす
+    $('.shinchoku_dodeska_tab_toggler').on('contextmenu', function(e) {
+        $shinchoku_dodeska_tab.removeClass('d-none');
+        e.preventDefault();
+    });
+    // 短めに押すと普通の進捗どうですか
+    $('.shinchoku_dodeska_tab_toggler').on('click', function(e) {
+        $shinchoku_dodeska_tab.addClass('d-none');
+    });
+    // スタンプを押すとスタンプタブが隠れる
+    $('.shinchoku_stamp').on('click', function(e) {
+        $shinchoku_dodeska_tab.addClass('d-none');
+    });
+
+    $('.shinchoku_dodeska_tab_toggler').hover(function(e) {
         $shinchoku_dodeska_tab.removeClass('d-none');
     }, function() {
         $shinchoku_dodeska_tab.addClass('d-none');
