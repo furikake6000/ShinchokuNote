03cd662ee3fda05a503f4459f25853c3b7c25d9d
furikake6000 <ewave63@keio.jp>
2019-04-30 23:16:30 +0900

    Refresh annotate gem

--- app/models/user_block.rb
+++ app/models/user_block.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: user_blocks
+#
+#  id           :integer          not null, primary key
+#  from_user_id :integer
+#  to_user_id   :integer
+#  to_addr      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class UserBlock < ApplicationRecord
 end
--- test/models/user_block_test.rb
+++ test/models/user_block_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: user_blocks
+#
+#  id           :integer          not null, primary key
+#  from_user_id :integer
+#  to_user_id   :integer
+#  to_addr      :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class UserBlockTest < ActiveSupport::TestCase
