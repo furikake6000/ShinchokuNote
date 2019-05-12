f7f8cff46c1763fd354d8300db291636ade7bd2d
furikake6000 <ewave63@keio.jp>
2019-05-12 19:07:58 +0900

    Added pathes of UserBlock

---
+++ app/controllers/user_blocks_controller.rb
@@ -0,0 +1,11 @@
+class UserBlocksController < ApplicationController
+  before_action :check_logged_in
+
+  def create
+
+  end
+
+  def delete
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,73 @@
 # == Route Map
 #
-#                    Prefix Verb   URI Pattern                                                                              Controller#Action
-#                      root GET    /                                                                                        users#home
-#                note_posts GET    /notes/:note_id/posts(.:format)                                                          posts#index
-#                           POST   /notes/:note_id/posts(.:format)                                                          posts#create
-#                      post DELETE /posts/:id(.:format)                                                                     posts#destroy
-#             note_comments POST   /notes/:note_id/comments(.:format)                                                       comments#create
-#                   comment GET    /comments/:id(.:format)                                                                  comments#show
-#                           PATCH  /comments/:id(.:format)                                                                  comments#update
-#                           PUT    /comments/:id(.:format)                                                                  comments#update
-#                           DELETE /comments/:id(.:format)                                                                  comments#destroy
-#                           GET    /notes/:note_id/comments(.:format)                                                       comments#index
-#          note_tweet_posts POST   /notes/:note_id/tweet_posts(.:format)                                                    tweet_posts#create
-#            note_schedules POST   /notes/:note_id/schedules(.:format)                                                      schedules#create
-#             edit_schedule GET    /schedules/:id/edit(.:format)                                                            schedules#edit
-#                  schedule PATCH  /schedules/:id(.:format)                                                                 schedules#update
-#                           PUT    /schedules/:id(.:format)                                                                 schedules#update
-#           note_watchlists POST   /notes/:note_id/watchlists(.:format)                                                     watchlists#create
-#                 watchlist DELETE /watchlists/:id(.:format)                                                                watchlists#destroy
-#    note_watchlists_toggle POST   /notes/:note_id/watchlists/toggle(.:format)                                              watchlists#toggle
-#             note_watchers GET    /notes/:note_id/watchers(.:format)                                                       notes#watchers
-#   note_shinchoku_dodeskas POST   /notes/:note_id/shinchoku_dodeskas(.:format)                                             shinchoku_dodeskas#create
-#                user_notes GET    /users/:user_id/notes(.:format)                                                          notes#index
-#                           POST   /users/:user_id/notes(.:format)                                                          notes#create
-#             new_user_note GET    /users/:user_id/notes/new(.:format)                                                      notes#new
-#                 edit_note GET    /notes/:id/edit(.:format)                                                                notes#edit
-#                      note GET    /notes/:id(.:format)                                                                     notes#show
-#                           PATCH  /notes/:id(.:format)                                                                     notes#update
-#                           PUT    /notes/:id(.:format)                                                                     notes#update
-#                           DELETE /notes/:id(.:format)                                                                     notes#destroy
-#                     users GET    /users(.:format)                                                                         users#index
-#                  new_user GET    /users/new(.:format)                                                                     users#new
-#                 edit_user GET    /users/:id/edit(.:format)                                                                users#edit
-#                      user GET    /users/:id(.:format)                                                                     users#show
-#                           PATCH  /users/:id(.:format)                                                                     users#update
-#                           PUT    /users/:id(.:format)                                                                     users#update
-#                           DELETE /users/:id(.:format)                                                                     users#destroy
-#                 announces GET    /announces(.:format)                                                                     announces#index
-#                           POST   /announces(.:format)                                                                     announces#create
-#                  announce GET    /announces/:id(.:format)                                                                 announces#show
-#                           PATCH  /announces/:id(.:format)                                                                 announces#update
-#                           PUT    /announces/:id(.:format)                                                                 announces#update
-#                           DELETE /announces/:id(.:format)                                                                 announces#destroy
-#                   devices POST   /devices(.:format)                                                                       devices#create
-#                    search GET    /search(.:format)                                                                        search#search
-#     auth_twitter_callback GET    /auth/twitter/callback(.:format)                                                         users#login
-#                    switch GET    /switch(.:format)                                                                        users#switchuser
-#                updateuser GET    /updateuser(.:format)                                                                    users#updateuser
-#         recommended_users GET    /recommended_users(.:format)                                                             users#recommended_users
-#                     login GET    /login(.:format)                                                                         users#new
-#                    logout GET    /logout(.:format)                                                                        users#logout
-#             notifications GET    /notifications(.:format)                                                                 users#notifications
-#                           POST   /notifications(.:format)                                                                 users#notifications_checked
-#                   omakase GET    /omakase(.:format)                                                                       notes#omakase
-#              newest_posts GET    /newest_posts(.:format)                                                                  posts#newest_posts
-#            watching_posts GET    /watching_posts(.:format)                                                                posts#watching_posts
-#                     about GET    /about(.:format)                                                                         static_pages#about
-#                       faq GET    /faq(.:format)                                                                           static_pages#faq
-#                      help GET    /help(.:format)                                                                          static_pages#help
-#                    manage GET    /manage(.:format)                                                                        static_pages#manage
-#                     terms GET    /terms(.:format)                                                                         static_pages#terms
-#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
-# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
-#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+#                         Prefix Verb   URI Pattern                                                                              Controller#Action
+#                           root GET    /                                                                                        users#home
+#                     note_posts GET    /notes/:note_id/posts(.:format)                                                          posts#index
+#                                POST   /notes/:note_id/posts(.:format)                                                          posts#create
+#                           post DELETE /posts/:id(.:format)                                                                     posts#destroy
+#                  note_comments POST   /notes/:note_id/comments(.:format)                                                       comments#create
+#                        comment GET    /comments/:id(.:format)                                                                  comments#show
+#                                PATCH  /comments/:id(.:format)                                                                  comments#update
+#                                PUT    /comments/:id(.:format)                                                                  comments#update
+#                                DELETE /comments/:id(.:format)                                                                  comments#destroy
+#                                GET    /notes/:note_id/comments(.:format)                                                       comments#index
+#               note_tweet_posts POST   /notes/:note_id/tweet_posts(.:format)                                                    tweet_posts#create
+#                 note_schedules POST   /notes/:note_id/schedules(.:format)                                                      schedules#create
+#                  edit_schedule GET    /schedules/:id/edit(.:format)                                                            schedules#edit
+#                       schedule PATCH  /schedules/:id(.:format)                                                                 schedules#update
+#                                PUT    /schedules/:id(.:format)                                                                 schedules#update
+#                note_watchlists POST   /notes/:note_id/watchlists(.:format)                                                     watchlists#create
+#                      watchlist DELETE /watchlists/:id(.:format)                                                                watchlists#destroy
+#         note_watchlists_toggle POST   /notes/:note_id/watchlists/toggle(.:format)                                              watchlists#toggle
+#                  note_watchers GET    /notes/:note_id/watchers(.:format)                                                       notes#watchers
+#        note_shinchoku_dodeskas POST   /notes/:note_id/shinchoku_dodeskas(.:format)                                             shinchoku_dodeskas#create
+# note_shinchoku_dodeskas_toggle POST   /notes/:note_id/shinchoku_dodeskas/toggle(.:format)                                      shinchoku_dodeskas#toggle
+#                     user_notes GET    /users/:user_id/notes(.:format)                                                          notes#index
+#                                POST   /users/:user_id/notes(.:format)                                                          notes#create
+#                  new_user_note GET    /users/:user_id/notes/new(.:format)                                                      notes#new
+#                      edit_note GET    /notes/:id/edit(.:format)                                                                notes#edit
+#                           note GET    /notes/:id(.:format)                                                                     notes#show
+#                                PATCH  /notes/:id(.:format)                                                                     notes#update
+#                                PUT    /notes/:id(.:format)                                                                     notes#update
+#                                DELETE /notes/:id(.:format)                                                                     notes#destroy
+#                          users GET    /users(.:format)                                                                         users#index
+#                       new_user GET    /users/new(.:format)                                                                     users#new
+#                      edit_user GET    /users/:id/edit(.:format)                                                                users#edit
+#                           user GET    /users/:id(.:format)                                                                     users#show
+#                                PATCH  /users/:id(.:format)                                                                     users#update
+#                                PUT    /users/:id(.:format)                                                                     users#update
+#                                DELETE /users/:id(.:format)                                                                     users#destroy
+#                      announces GET    /announces(.:format)                                                                     announces#index
+#                                POST   /announces(.:format)                                                                     announces#create
+#                       announce GET    /announces/:id(.:format)                                                                 announces#show
+#                                PATCH  /announces/:id(.:format)                                                                 announces#update
+#                                PUT    /announces/:id(.:format)                                                                 announces#update
+#                                DELETE /announces/:id(.:format)                                                                 announces#destroy
+#                        devices POST   /devices(.:format)                                                                       devices#create
+#                    user_blocks POST   /user_blocks(.:format)                                                                   user_blocks#create
+#                     user_block DELETE /user_blocks/:id(.:format)                                                               user_blocks#destroy
+#                         search GET    /search(.:format)                                                                        search#search
+#          auth_twitter_callback GET    /auth/twitter/callback(.:format)                                                         users#login
+#                         switch GET    /switch(.:format)                                                                        users#switchuser
+#                     updateuser GET    /updateuser(.:format)                                                                    users#updateuser
+#              recommended_users GET    /recommended_users(.:format)                                                             users#recommended_users
+#                          login GET    /login(.:format)                                                                         users#new
+#                         logout GET    /logout(.:format)                                                                        users#logout
+#                  notifications GET    /notifications(.:format)                                                                 users#notifications
+#                                POST   /notifications(.:format)                                                                 users#notifications_checked
+#                        omakase GET    /omakase(.:format)                                                                       notes#omakase
+#                   newest_posts GET    /newest_posts(.:format)                                                                  posts#newest_posts
+#                 watching_posts GET    /watching_posts(.:format)                                                                posts#watching_posts
+#                          about GET    /about(.:format)                                                                         static_pages#about
+#                            faq GET    /faq(.:format)                                                                           static_pages#faq
+#                           help GET    /help(.:format)                                                                          static_pages#help
+#                         manage GET    /manage(.:format)                                                                        static_pages#manage
+#                          terms GET    /terms(.:format)                                                                         static_pages#terms
+#             rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+#      rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#             rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+#      update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#           rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
 
 Rails.application.routes.draw do
   # トップページ
@@ -110,6 +113,9 @@ Rails.application.routes.draw do
 
   # リソース:デバイス(Webpushに使用)
   resources :devices, only: [:create]
+
+  # リソース:ユーザーブロック
+  resources :user_blocks, only: %i[create destroy]
 
   get '/search', to: 'search#search'
   get '/auth/twitter/callback', to: 'users#login'
