8f04afb26606919f376190cb0c81c6b5bd674a84
furikake6000 <ewave63@keio.jp>
2019-08-03 22:07:24 +0900

    コメントテキストボックスが初期で埋まっている場合に文字数カウンタやボタンに反映するように

--- app/assets/javascripts/comment_text_box.js
+++ app/assets/javascripts/comment_text_box.js
@@ -1,18 +1,22 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('turbolinks:load', function () {
     $commenttext_form = $("#commenttext_form");
     $commenttext_count = $("#commenttext_count");
     $comment_submit = $("#comment_submit");
 
     var maxtextlen = 1000;
 
-    $commenttext_form.keyup(function() {
+    refreshTextbox();
+
+    $commenttext_form.keyup(refreshTextbox);
+
+    function refreshTextbox() {
         var textlen = $commenttext_form.val().length;
         $commenttext_count.text(textlen + " / " + maxtextlen);
 
-        if(textlen == 0 || textlen > maxtextlen){
+        if (textlen == 0 || textlen > maxtextlen) {
             $comment_submit.prop("disabled", true);
-        }else{
+        } else {
             $comment_submit.prop("disabled", false);
         }
-    });
+    }
 })
