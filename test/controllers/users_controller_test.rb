da91a2d024b54a4133dcfd1e9a8c02ec935af2c6
furikake6000 <ewave63@keio.jp>
2020-05-03 01:26:25 +0900

    users_controller_testでassertしているtokenとsecretを修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -144,12 +144,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_nil master_user_secret
     # Token and secret with logging in
     login_as_noritama
-    assert_equal master_user_token, Rails.application.credentials.twitter_test_fixture[:noritama_token]
-    assert_equal master_user_secret, Rails.application.credentials.twitter_test_fixture[:noritama_secret]
+    assert_equal master_user_token, 'noritama_token'
+    assert_equal master_user_secret, 'noritama_secret'
     # Token and secret with logging in with two accounts
     login_as_okaka
-    assert_equal master_user_token, Rails.application.credentials.twitter_test_fixture[:noritama_token]
-    assert_equal master_user_secret, Rails.application.credentials.twitter_test_fixture[:noritama_secret]
+    assert_equal master_user_token, 'noritama_token'
+    assert_equal master_user_secret, 'noritama_secret'
   end
 
   test 'get current_user_token and secret' do
@@ -158,12 +158,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_nil current_user_secret
     # Token and secret with logging in
     login_as_noritama
-    assert_equal current_user_token, Rails.application.credentials.twitter_test_fixture[:noritama_token]
-    assert_equal current_user_secret, Rails.application.credentials.twitter_test_fixture[:noritama_secret]
+    assert_equal current_user_token, 'noritama_token'
+    assert_equal current_user_secret, 'noritama_secret'
     # Token and secret with logging in with two accounts
     login_as_okaka
-    assert_equal current_user_token, Rails.application.credentials.twitter_test_fixture[:okaka_token]
-    assert_equal current_user_secret, Rails.application.credentials.twitter_test_fixture[:okaka_secret]
+    assert_equal current_user_token, 'okaka_token'
+    assert_equal current_user_secret, 'okaka_secret'
   end
 
   test 'delete user' do
