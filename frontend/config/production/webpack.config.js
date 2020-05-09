12bc2fe150f71eea2429bd80a31b5f9dcd168e24
furikake6000 <ewave63@keio.jp>
2020-05-09 14:27:04 +0900

    webpack.configとスクリプトの作成

---
+++ frontend/config/production/webpack.config.js
@@ -0,0 +1,50 @@
+// ref: https://qiita.com/okabee326/items/014c282a24b0886c814e#webpack%E3%82%92%E5%B0%8E%E5%85%A5%E3%81%99%E3%82%8B
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    webpack: './src/javascripts/entry.js'
+  },
+  output: {
+    path: '/ShinchokuNote/app/assets/javascripts',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader'
+          },
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
