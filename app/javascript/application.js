ff99aa810110988a96824829a7df45b89bb54da9
furikake6000 <ewave63@keio.jp>
2020-05-16 01:08:25 +0900

    Vue用のサンプルスクリプトを記述

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-import { hello } from "./greeter";
+import Hello from './components/pages/Hello.vue';
+import Vue from 'vue';
 
-console.log(hello("Rails"));
+document.addEventListener('DOMContentLoaded', () => {
+  // Vueが使われていないページでは無効化
+  if (document.getElementById('app') == null) {
+    return;
+  }
+
+  new Vue({
+    el: '#app',
+    components: { Hello }
+  });
+});
