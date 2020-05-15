abf7d94611b9e6e0cfb42b0e4f3dcc980dffbd38
furikake6000 <ewave63@keio.jp>
2020-05-15 23:00:00 +0900

    webpack.configのdouble quoteを修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const WebpackAssetsManifest = require("webpack-assets-manifest");
+const path = require('path');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
 
 const { NODE_ENV } = process.env;
-const isProd = NODE_ENV === "production";
+const isProd = NODE_ENV === 'production';
 
 module.exports = {
-  mode: isProd ? "production" : "development",
-  devtool: "source-map",
+  mode: isProd ? 'production' : 'development',
+  devtool: 'source-map',
   entry: {
-    application: path.resolve(__dirname, "app/javascript/application.js")
+    application: path.resolve(__dirname, 'app/javascript/application.js')
   },
   output: {
-    path: path.resolve(__dirname, "public/packs"),
-    publicPath: "/packs/",
-    filename: isProd ? "[name]-[hash].js" : "[name].js"
+    path: path.resolve(__dirname, 'public/packs'),
+    publicPath: '/packs/',
+    filename: isProd ? '[name]-[hash].js' : '[name].js'
   },
   resolve: {
-    extensions: [".js"]
+    extensions: ['.js']
   },
   plugins: [new WebpackAssetsManifest({ publicPath: true })]
 };
