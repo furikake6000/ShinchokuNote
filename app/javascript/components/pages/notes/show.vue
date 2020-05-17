2a1e7c4e51be0eed36fdf456a1444b0d865bb727
furikake6000 <ewave63@keio.jp>
2020-05-17 23:19:33 +0900

    ノートタイトル欄のmock作成

--- app/javascript/components/pages/notes/show.vue
+++ app/javascript/components/pages/notes/show.vue
@@ -1,7 +1,29 @@
-<template>
-  <v-app>
-    
-  </v-app>
+<template lang="pug">
+  v-app
+    main
+      .cover
+        v-container
+          h1.name.mt-4.pb-3
+            .d-flex.flex-column.flex-md-row
+              div.flex-grow-1
+                span test
+                v-chip(color="success" outlined).ml-2 完成
+                v-chip(color="secondary" outlined).ml-2
+                  v-icon mdi-lock
+                  span 自分のみ
+                v-chip(color="error" outlined).ml-2 R18
+              .text-left
+                v-btn(rounded color="secondary").align-self-center.follow-btn 編集する
+          .d-flex.flex-column.flex-md-row.mt-4
+            p.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. 
+            .flex-shrink-0.text-right.align-self-end
+              .secondary--text.text--lighten-1.font-weight-bold 2019年1月1日作成 (100日め)
 </template>
 
 <script>
@@ -9,5 +31,27 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang="sass">
+#app
+  font-family: -apple-system, blinkMacSystemFont, 'Helvetica Neue', 'Segoe UI', 'Yu Gothic', YuGothic, Meiryo, sans-serif
+  background-color: #FFF6F3
+
+.container
+  @media (min-width: 1264px)
+    max-width: 1185px
+
+.cover
+  background-color: white
+  h1.name
+    font-size: 48px
+    color: var(--v-primary-base)
+    border-color: var(--v-primary-base)
+    span.v-chip
+      letter-spacing: initial
+
+button.v-btn.follow-btn
+  width: 180px
+  height: 48px
+  font-size: 16px
+  font-weight: bold
 </style>
