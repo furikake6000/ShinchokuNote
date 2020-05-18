50b02b79c8df287c9bdbd69f808fd145ba04e93e
furikake6000 <ewave63@keio.jp>
2020-05-18 13:57:23 +0900

    「最近のノート」部分のmock

--- app/javascript/components/pages/notes/show.vue
+++ app/javascript/components/pages/notes/show.vue
@@ -27,7 +27,25 @@
     v-container
       v-row
         v-col(cols="8")
-          p hi!
+          .new-post-form
+            .new-post-text-form
+              textarea(placeholder="新しい進捗を投稿する...")
+              .image-hint テキストエリアへの画像のドロップ・コピペが可能です
+            .d-flex.align-center.mt-2
+              v-btn(icon color="secondary")
+                v-icon mdi-image
+              v-tooltip(top)
+                template(v-slot:activator="{ on }")
+                  v-btn(icon color="secondary" v-on="on")
+                    v-icon mdi-link
+                span URLでリンク
+              v-tooltip(top)
+                template(v-slot:activator="{ on }")
+                  v-btn(icon color="secondary" v-on="on")
+                    v-icon mdi-calendar
+                span スケジュールの追加
+              span.secondary--text.subtitle-1.font-weight-bold.ml-auto.mr-4 0 / 1000
+              v-btn(rounded color="primary").follow-btn 投稿する
         v-col(cols="4")
           .user-info
             v-avatar(size="100px")
@@ -43,8 +61,71 @@
               br
               a.body-2.primary--text(href="https://example.com") https://example.com
             .user-notes
-              .text-center.secondary--text.text--lighten-1 最近のノート
-            v-btn(text block color="primary") マイページへ
+              .text-center.secondary--text 最近のノート
+              v-card.note-card.my-4
+                .bgstr
+                  v-icon mdi-note-text
+                .note-card-content
+                  v-card-title.headline
+                    span.mr-2 test
+                    .chips
+                      v-chip(color="success" small).ml-1 完成
+                      v-chip(color="secondary" small).ml-1
+                        v-icon mdi-lock
+                        span 自分のみ
+                      v-chip(color="error" small).ml-1 R18
+                  v-card-subtitle Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ...
+                  v-card-actions                
+                    v-spacer
+                    v-tooltip(top)
+                      template(v-slot:activator="{ on }")
+                        v-btn(text color="secondary" v-on="on")
+                          v-icon mdi-star
+                          span 10
+                      span フォローする
+              v-card.note-card.my-4
+                .bgstr
+                  v-icon mdi-note-text
+                .note-card-content
+                  v-img(
+                    src="http://localhost:3000/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCZz09IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--f97e59db7026b209deb6890df4bc5551512f79f5/blob"
+                    height="200px"
+                    gradient="to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, .5)"
+                  ).white--text.align-end
+                    v-card-title.headline
+                      span.mr-2 test
+                      .chips
+                        v-chip(color="error" small).ml-1 R18
+                  v-card-subtitle Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ...
+                  v-card-actions                
+                    v-spacer
+                    v-tooltip(top)
+                      template(v-slot:activator="{ on }")
+                        v-btn(text color="secondary" v-on="on")
+                          v-icon mdi-star
+                          span 10
+                      span フォローする
+              .text-center.secondary--text リクエストボックス
+              v-card.note-card.my-4
+                .bgstr.request
+                  v-icon mdi-email
+                .note-card-content
+                  v-card-title.headline
+                    span.mr-2 test
+                    .chips
+                      v-chip(color="secondary" small).ml-2
+                        v-icon mdi-lock
+                        span フォロワーさんのみ
+                  v-card-subtitle Lorem ipsum dolor sit amet...
+                  v-card-actions                
+                    v-spacer
+                    v-tooltip(top)
+                      template(v-slot:activator="{ on }")
+                        v-btn(text color="secondary" v-on="on")
+                          v-icon mdi-star
+                          span 10
+                      span フォローする
+            v-btn(outlined block color="primary") マイページへ
 </template>
 
 <script>
@@ -80,9 +161,57 @@ export default {
   .user-desc
     border-bottom: 5px dotted var(--v-secondary-lighten2)
 
+.new-post-form
+  .new-post-text-form
+    position: relative
+    textarea
+      width: 100%
+      padding: 20px
+      background-color: white
+      border: 2px solid var(--v-secondary-lighten2)
+      border-radius: 20px
+      &::placeholder
+        color: var(--v-secondary-lighten2)
+        font-weight: bold
+    .image-hint
+      position: absolute
+      right: 20px
+      bottom: 20px
+      color: var(--v-secondary-lighten2)
+      font-weight: bold
+      font-size: 0.8rem
+
+.note-card
+  position: relative
+  overflow: hidden
+  .bgstr
+    position: absolute
+    z-index: 0
+    bottom: -80px
+    right: -20px
+    white-space: nowrap
+    font-size: 14rem
+    font-weight: bold
+    transform: rotate(-30deg)
+    color: var(--v-primary-lighten5)
+    &.request
+      color: var(--v-accent-lighten2)
+    .v-icon
+      font-size: unset
+      color: unset
+  .note-card-content
+    position: relative
+    z-index: 1
+
 button.v-btn.follow-btn
   width: 12rem
   height: 3rem
+  font-size: 1rem
+  font-weight: bold
+
+button.v-btn.follow-btn
+  width: 8rem
+  height: 2.5rem
   font-size: 1rem
   font-weight: bold
 </style>
