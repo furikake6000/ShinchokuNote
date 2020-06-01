7ec151d3e29c3bcfa8813835557b796be9cbe352
furikake6000 <ewave63@keio.jp>
2020-06-01 20:11:54 +0900

    ボタンの配置とundo, redo, resetの実装

--- app/javascript/components/modules/posts/image_editor.vue
+++ app/javascript/components/modules/posts/image_editor.vue
@@ -1,6 +1,26 @@
 <template lang="pug">
   .image-editor-gui
     #image_editor.d-flex.justify-center
+    .ma-4.d-flex.justify-center
+      v-btn(@click="undo" icon text large)
+        v-icon mdi-undo
+      v-btn(@click="redo" icon text large)
+        v-icon mdi-redo
+      v-btn(@click="reset" icon text large)
+        v-icon mdi-restore
+      v-icon.mx-2.secondary--text(large) mdi-power-on
+      v-btn(icon text large)
+        v-icon mdi-crop
+      v-btn(icon text large)
+        v-icon mdi-flip-horizontal
+      v-btn(icon text large)
+        v-icon mdi-tune
+      v-icon.mx-2.secondary--text(large) mdi-power-on
+      v-btn(icon text large color="success lighten-3")
+        v-icon mdi-check
+      v-btn(icon text large color="error lighten-3")
+        v-icon mdi-close
+
 </template>
 
 <script>
@@ -21,6 +41,15 @@ export default {
       this.imageEditor.loadImageFromURL(image, "image").then((result) =>{
         this.imageEditor.clearUndoStack();
       });
+    },
+    undo() {
+      this.imageEditor.undo();
+    },
+    redo() {
+      this.imageEditor.redo();
+    },
+    reset() {
+      this.loadImage(this.image);
     }
   },
   watch: {
