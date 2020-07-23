52be07f950818350b3ce0530b138eae39a7d9077
furikake6000 <ewave63@keio.jp>
2020-07-23 14:16:25 +0900

    post_testで不要になったfixtureのsetupを削除

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -19,14 +19,6 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  def setup
-    @okaka = users(:okaka)
-    @noritama = users(:noritama)
-    @okaka_project1 = notes(:okaka_project_1)
-    @noritama_project1 = notes(:noritama_project_1)
-    @okaka_tweet_post1 = posts(:okaka_tweet_post_1)
-  end
-
   test 'valid post' do
     post = create(:post)
     assert post.valid?
