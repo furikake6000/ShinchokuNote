2a558fecbaebf8fe146f849e037766a029c6ddd8
furikake6000 <ewave63@keio.jp>
2020-07-23 14:26:01 +0900

    note_testのFactoryBotへの置換

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -25,34 +25,29 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  def setup
-    @okaka = users(:okaka)
-    @noritama = users(:noritama)
-    @okaka_project1 = notes(:okaka_project_1)
-    @okaka_project2 = notes(:okaka_project_2)
-    @noritama_project1 = notes(:noritama_project_1)
-  end
-
   test 'invalid note uniqueness' do
-    @okaka_project1.save
+    user = create(:user)
+    project = create(:project, user: user)
     # Same user cant have same name note
-    @okaka_project2.name = @okaka_project1.name
-    assert_not @okaka_project2.valid?
+    same_name_project = build(:project, user: user, name: project.name)
+    assert_not same_name_project.valid?
     # Other user can have same name note
-    @noritama_project1.name = @okaka_project1.name
-    assert @noritama_project1.valid?
+    same_name_other_user_project = build(:project, name: project.name)
+    assert same_name_other_user_project.valid?
   end
 
   test 'invalid note no_type' do
     # Type "" means plain note, but it isn't allowed.
-    @okaka_project1.type = ''
-    assert_not @okaka_project1.valid?
+    project = build(:project)
+    project.type = ''
+    assert_not project.valid?
   end
 
   test 'invalid note wrong_type' do
     # Setting wrong type raises ArgumentError.
+    project = build(:project)
     assert_raises(ArgumentError) do
-      @okaka_project1.type = 'Undefined'
+      project.type = 'Undefined'
     end
   end
 end
