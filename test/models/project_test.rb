ce87ceb8edfddd5b91c32a2c1237dc50b797c1a7
furikake6000 <ewave63@keio.jp>
2020-07-23 14:28:23 +0900

    project testをFactoryBotに置換

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -25,20 +25,13 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  def setup
-    @okaka = users(:okaka)
-    @noritama = users(:noritama)
-    @okaka_project1 = notes(:okaka_project_1)
-    @okaka_project2 = notes(:okaka_project_2)
-    @noritama_project1 = notes(:noritama_project_1)
-  end
-
   test 'valid project' do
-    assert @okaka_project1.valid?
+    assert create(:project).valid?
   end
 
   test 'invalid project no_name' do
-    @okaka_project1.name = ''
-    assert_not @okaka_project1.valid?
+    project = create(:project)
+    project.name = ''
+    assert_not project.valid?
   end
 end
