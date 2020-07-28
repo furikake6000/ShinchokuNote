bf2e6441b420208354f081b7a1e15e5c355512e2
furikake6000 <ewave63@keio.jp>
2020-07-28 23:19:31 +0900

    GET /noteの404, 403のテストを追加

--- test/controllers/api/v1/notes_controller_test.rb
+++ test/controllers/api/v1/notes_controller_test.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class NotesControllerTest < ActionDispatch::IntegrationTest
       include Committee::Rails::Test::Methods
+      include UsersHelper
 
       def setup
         @project = create(:project)
@@ -16,7 +17,42 @@ module Api
       test '正常なリクエスト' do
         get api_v1_note_path(@project)
         assert_response 200
-        assert_schema_conform
+        assert_response_schema_confirm
+        
+        r = JSON.parse(response.body)
+        assert_equal r['type'], 'Project'
+        assert_equal r['name'], @project.name
+        assert_equal r['desc'], @project.desc
+        assert_equal r['stage'], @project.stage
+        assert_equal r['view_stance'], @project.view_stance
+        assert_equal r['rating'], @project.rating
+        assert_equal r['url'], note_path(@project)
+        assert_equal r['watch_url'], note_watchlists_toggle_path(@project)
+        assert_equal r['watchers_count'], @project.watching_users.count
+        assert_equal r['shinchoku_dodeskas_count'], @project.shinchoku_dodeskas.count
+        assert_equal r['comments_count'], @project.comments.count
+        assert_equal DateTime.parse(r['created_at']).to_i, @project.created_at.to_i
+      end
+
+      test '存在しないnoteへのリクエスト' do
+        @project.destroy!
+
+        get api_v1_note_path(@project)
+        assert_response 404
+      end
+
+      test 'アクセス不可なnoteへのリクエスト' do
+        @project.only_me_view_stance!
+
+        # 未ログイン時403
+        get api_v1_note_path(@project)
+        assert_response 403
+
+        login_for_test @project.user
+
+        # 作成者ログイン時200
+        get api_v1_note_path(@project)
+        assert_response 200
       end
     end
   end
