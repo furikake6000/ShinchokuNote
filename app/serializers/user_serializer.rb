847dcfdc311ec15884d45d717e2892eb6c9e3d78
furikake6000 <ewave63@keio.jp>
2020-07-29 23:42:50 +0900

    serializerにannotate追加

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                                :bigint(8)        not null, primary key
+#  twitter_id                        :string
+#  name                              :string
+#  screen_name                       :string
+#  url                               :string
+#  thumb_url                         :string
+#  desc                              :string
+#  user_group_info                   :string
+#  permission                        :string           default("")
+#  deleted_at                        :datetime
+#  created_at                        :datetime         not null
+#  updated_at                        :datetime         not null
+#  checked_notifications_at          :datetime
+#  linked_users_info                 :binary
+#  saw_notifications_at              :datetime
+#  comment_webpush_enabled           :boolean          default(TRUE)
+#  shinchoku_dodeska_webpush_enabled :boolean          default(TRUE)
+#
+
 class UserSerializer < ActiveModel::Serializer
   attributes :name,
              :thumb_url,
