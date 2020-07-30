baaed421da5391cf4561c69f0c8e43d2d4853a00
furikake6000 <ewave63@keio.jp>
2020-07-30 23:15:06 +0900

    GET /note/{id}でnot foundとforbiddenの処理を実装

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,7 +2,24 @@ module Api
   module V1
     class NotesController < ApplicationController
       def show
-        @note = Note.find_by(id: params[:id])
+        begin
+          @note = Note.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          render json: {
+            code: 'note_not_found',
+            message: 'ノートが見つかりませんでした。'
+          }, status: :not_found
+          return
+        end
+
+        unless user_can_see? @note, current_user
+          render json: {
+            code: 'note_forbidden',
+            message: 'ノートの閲覧権限がありません。'
+          }, status: :forbidden
+          return
+        end
+
         render json: @note, include: { user: %i[projects request_boxes] }
       end
     end
