5084a847a1c0b44d7fe02129f34bfbfcac2a943a
furikake6000 <ewave63@keio.jp>
2020-08-01 00:18:14 +0900

    GET postsの内容のassertionを追加

--- test/controllers/api/v1/posts_controller_test.rb
+++ test/controllers/api/v1/posts_controller_test.rb
@@ -15,6 +15,15 @@ module Api
         get api_v1_note_posts_path(@project)
         assert_response 200
         assert_response_schema_confirm
+        
+        r = JSON.parse(response.body)
+        assert r.has_key?('posts')
+        
+        post_hash = r['posts'].first
+        post = Post.find(post_hash['id'])
+        assert_equal post_hash['type'], post.type
+        assert_equal post_hash['text'], post.text
+        assert_equal DateTime.parse(post_hash['date']).to_i, post.created_at.to_i
       end
 
       test 'GET /notes/{id}/posts 存在しないnoteへのリクエスト' do
