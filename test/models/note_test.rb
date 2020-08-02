70c280ad47461de3036f3b46a3bd28f419f8f20a
furikake6000 <ewave63@keio.jp>
2020-08-02 15:11:27 +0900

    Noteの更新日ソートに関するテストの追加

--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -50,4 +50,63 @@ class NoteTest < ActiveSupport::TestCase
       project.type = 'Undefined'
     end
   end
+
+  test '投稿すると更新日が更新される' do
+    today = Time.current
+    yesterday = today.yesterday
+
+    note = nil
+    Timecop.freeze(yesterday) do
+      note = create(:project)
+    end
+    assert_equal note.updated_at, yesterday
+
+    post = nil
+    Timecop.freeze(today) do
+      post = create(:post, note: note)
+    end
+    assert_equal note.updated_at, today
+  end
+
+  test 'user.noteが作成日降順に並ぶ' do
+    user = create(:user)
+    current_note = create(:project, user: user)
+    old_note, new_note = nil
+    Timecop.freeze(Time.current.yesterday) do
+      old_note = create(:project, user: user)
+    end
+    Timecop.freeze(Time.current.tomorrow) do
+      new_note = create(:project, user: user)
+    end
+
+    assert_equal user.notes.first.id, new_note.id
+    assert_equal user.notes.second.id, current_note.id
+    assert_equal user.notes.third.id, old_note.id
+  end
+
+  test 'user.noteが更新日降順に並ぶ' do
+    user = create(:user)
+    old_note, older_note = nil
+    Timecop.freeze(Time.current.yesterday) do
+      old_note = create(:project, user: user)
+    end
+    Timecop.freeze(Time.current.ago(2.days)) do
+      older_note = create(:project, user: user)
+    end
+
+    assert_equal user.notes.first.id, old_note.id
+    assert_equal user.notes.second.id, older_note.id
+
+    # 新規投稿をすると順番が入れ替わる
+
+    create(:post, note: older_note)
+
+    assert_equal user.notes.first.id, older_note.id
+    assert_equal user.notes.second.id, old_note.id
+
+    create(:post, note: old_note)
+
+    assert_equal user.notes.first.id, old_note.id
+    assert_equal user.notes.second.id, older_note.id
+  end
 end
