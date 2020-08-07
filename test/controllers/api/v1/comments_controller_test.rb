fbb5217823327365bb6096996d75d799dc993fa2
furikake6000 <ewave63@keio.jp>
2020-08-08 00:35:35 +0900

    GET /notes/{id}/commentsのテストを作成

---
+++ test/controllers/api/v1/comments_controller_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+module Api
+  module V1
+    class CommentsControllerTest < ActionDispatch::IntegrationTest
+      include UsersHelper
+
+      def setup
+        @project = create(:project)
+      end
+
+      # GET /notes/{id}/comments
+
+      test 'GET /notes/{id}/comments 正常なリクエスト' do
+        get api_v1_note_comments_path(@project)
+        assert_response 200
+        assert_response_schema_confirm
+        
+        r = JSON.parse(response.body)
+        assert r.has_key?('comments')
+        
+        comment_hash = r['comments'].first
+        comment = Comment.find(comment_hash['id'])
+        assert_equal comment_hash['text'], comment.text
+        assert_equal DateTime.parse(comment_hash['date']).to_i, comment.created_at.to_i
+      end
+
+      test 'GET /notes/{id}/comments 存在しないnoteへのリクエスト' do
+        @project.destroy!
+
+        get api_v1_note_comments_path(@project)
+        assert_response 404
+      end
+
+      test 'GET /notes/{id}/comments アクセス不可なnoteへのリクエスト' do
+        @project.only_me_view_stance!
+
+        # 未ログイン時403
+        get api_v1_note_comments_path(@project)
+        assert_response 403
+
+        login_for_test @project.user
+
+        # 作成者ログイン時200
+        get api_v1_note_comments_path(@project)
+        assert_response 200
+      end
+    end
+  end
+end
