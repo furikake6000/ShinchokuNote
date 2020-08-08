e25ff22d3f6209814ecbf4be7fb80668a0e158e5
furikake6000 <ewave63@keio.jp>
2020-08-08 17:29:05 +0900

    commentscontrollerの実装

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def index
+        begin
+          @note = Note.find(params[:note_id])
+        rescue ActiveRecord::RecordNotFound
+          render json: {
+            code: 'note_not_found',
+            message: 'ノートが見つかりませんでした。'
+          }, status: :not_found
+          return
+        end
+
+        unless user_can_see? @note, current_user
+          render json: {
+            code: 'note_forbidden',
+            message: 'ノートの閲覧権限がありません。'
+          }, status: :forbidden
+          return
+        end
+
+        @comments = @note.comments
+        render json: @comments, root: 'comments', adapter: :json
+      end
+    end
+  end
+end
