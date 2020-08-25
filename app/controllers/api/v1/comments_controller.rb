e72917fa56461ae89a3a15c984d99aa1183d9d67
furikake6000 <ewave63@keio.jp>
2020-08-25 13:52:41 +0900

    GET commentsエンドポイントがfilterパラメータを考慮するように

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -20,11 +20,22 @@ module Api
           return
         end
 
-        @comments = @note.comments.page(params[:page] || 1)
-        render json: @comments, root: 'comments', adapter: :json, meta: {
-          current_page: @comments.current_page,
-          total_pages: @comments.total_pages,
-          count: @comments.limit_value,
+        case params[:filter]
+        when 'replied'
+          @comments = @note.comments.replied
+        when 'unreplied'
+          @comments = @note.comments.unreplied
+        when 'favored'
+          @comments = @note.comments.favored
+        else
+          @comments = @note.comments
+        end
+
+        @comments_page = @comments.page(params[:page] || 1)
+        render json: @comments_page, root: 'comments', adapter: :json, meta: {
+          current_page: @comments_page.current_page,
+          total_pages: @comments_page.total_pages,
+          count: @comments_page.size,
           total_count: @note.comments.count
         }
       end
