8277a1228000174b098c2b43052472eee4651e2c
furikake6000 <ewave5000@gmail.com>
2020-12-21 00:21:05 +0900

    GET /commentsで403が出た場合、「コメントは非公開です」と表示するように

--- app/javascript/components/modules/comments/comments.vue
+++ app/javascript/components/modules/comments/comments.vue
@@ -2,29 +2,35 @@
   .comments
     .bgstr.top
       v-icon(color="secondary lighten-3") mdi-email
-    .comments-title.secondary--text.font-weight-bold コメント({{totalCount}})
+    .comments-title.secondary--text.font-weight-bold コメント
+      span(v-if="commentsVisibility") ({{totalCount}})
     comment-form.mt-2(@refreshComments="fetchComments()")
     v-divider
-    v-btn-toggle(borderless dense mandatory v-model="filter")
-      v-btn(value="all") すべて
-      v-btn(value="unreplied") 未返信
-      v-btn(value="favored")
-        v-icon(small color="secondary darken-3") mdi-heart
-        span お気に入り
+    template(v-if="commentsVisibility")
+      v-btn-toggle(borderless dense mandatory v-model="filter")
+        v-btn(value="all") すべて
+        v-btn(value="unreplied") 未返信
+        v-btn(value="favored")
+          v-icon(small color="secondary darken-3") mdi-heart
+          span お気に入り
 
-    v-pagination.my-4(
-      v-model="currentPage" circle
-      :length="totalPages"
-      :total-visible="9"
-      v-if="totalPages > 1"
-    )
-    comment.my-4(v-bind="comment" v-for="comment in filteredComments" :key="comment.id")
-    v-pagination.my-4(
-      v-model="currentPage" circle
-      :length="totalPages"
-      :total-visible="9"
-      v-if="totalPages > 1"
-    )
+      v-pagination.my-4(
+        v-model="currentPage" circle
+        :length="totalPages"
+        :total-visible="9"
+        v-if="totalPages > 1"
+      )
+      comment.my-4(v-bind="comment" v-for="comment in filteredComments" :key="comment.id")
+      v-pagination.my-4(
+        v-model="currentPage" circle
+        :length="totalPages"
+        :total-visible="9"
+        v-if="totalPages > 1"
+      )
+    template(v-else)
+      .my-12.text-center
+        v-icon(x-large color="secondary") mdi-lock
+        h5.secondary--text コメントは非公開です。
 </template>
 
 <script>
@@ -42,7 +48,8 @@ export default {
       totalCount: 0,
       currentPage: 1,
       totalPages: 1,
-      filter: 'all'
+      filter: 'all',
+      commentsVisibility: true
     };
   },
   computed: {
@@ -69,6 +76,11 @@ export default {
         this.currentPage = data.meta.currentPage;
         this.totalCount = data.meta.totalCount;
         this.totalPages = data.meta.totalPages;
+      }).catch(error => {
+        console.log(error.response);
+        if(error.response.status == 403) {
+          this.commentsVisibility = false;
+        }
       });
     }
   },
