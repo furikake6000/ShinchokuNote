2c0de1d99c39da0882aa583b6792ed5bf148d32c
furikake6000 <ewave5000@gmail.com>
2021-02-10 00:51:45 +0900

    reCAPTCHA v3の認証に失敗したらreCAPTCHA v2のcheckboxを出すように

--- app/javascript/components/modules/comments/comment_form.vue
+++ app/javascript/components/modules/comments/comment_form.vue
@@ -12,26 +12,38 @@
       v-btn(@click="postComment" rounded color="primary" :disabled="text.length == 0").follow-btn.font-weight-bold コメントする
 
       v-snackbar(v-model="snackbarEnabled" timeout=3000) {{snackbarText}}
+    vue-recaptcha(v-if="recaptchaV2Enabled" :sitekey="recaptchaKeyV2")
 </template>
 
 <script>
+import VueRecaptcha from 'vue-recaptcha'
 import { load } from 'recaptcha-v3'
 
-const recaptchaKey = '6Lf4JqgUAAAAANnrDZwns_XB_Sw0Vm3KdSKROLZk'
+const recaptchaKeyV2 = '6LejIbEUAAAAADO263cfRghHFoAvL_SuykU5tfV2'
+const recaptchaKeyV3 = '6Lf4JqgUAAAAANnrDZwns_XB_Sw0Vm3KdSKROLZk'
 
 export default {
   name: 'comment-form',
+  components: {
+    VueRecaptcha
+  },
   data: () => {
     return {
       text: '',
       showAuthor: false,
       snackbarEnabled: false,
       snackbarText: '',
-      recaptcha: null
+      recaptchaV3: null,
+      recaptchaV2Enabled: false
     };
   },
   async created() {
-    this.recaptcha = await load(recaptchaKey)
+    // reCAPTCHA v2のスクリプトをロード
+    const recaptchaV2Script = document.createElement('script')
+    recaptchaV2Script.setAttribute('src', 'https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit');
+    document.body.appendChild(recaptchaV2Script);
+
+    this.recaptchaV3 = await load(recaptchaKeyV3)
   },
   computed: {
     anonimity() {
@@ -42,11 +54,12 @@ export default {
         text: this.text,
         anonimity: this.anonimity
       }
-    }
+    },
+    recaptchaKeyV2: () => recaptchaKeyV2
   },
   methods: {
     postComment() {
-      this.recaptcha.execute('social').then(token => {
+      this.recaptchaV3.execute('social').then(token => {
         this.axios.post(`/api/v1/notes/${ this.$route.params.id }/comments`, {
           comment: this.newComment,
           recaptcha: token
@@ -57,7 +70,14 @@ export default {
           this.showAuthor = false
         })
         .catch((error) => {
-          this.showSnackbar(error.response.data.message)
+          switch(error.response.data.code) {
+            case 'recaptcha_failed':
+              this.showSnackbar('「私はロボットではありません」をチェックして下さい。')
+              this.recaptchaV2Enabled = true
+              break
+            default:
+              this.showSnackbar(error.response.data.message)
+          }
         })
         .then(() => {
           // always executed
