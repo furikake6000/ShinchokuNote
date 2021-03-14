6bf7d77c4719cff45361b95061f6cc1912e10f97
furikake6000 <ewave5000@gmail.com>
2021-03-14 13:46:46 +0900

    watchlists_controllerの処理を実装

--- app/controllers/api/v1/watchlists_controller.rb
+++ app/controllers/api/v1/watchlists_controller.rb
@@ -3,13 +3,41 @@ module Api
     class WatchlistsController < ApplicationController
       before_action :load_note
       before_action :check_note_authority
+      before_action :load_watchlist
 
       def create
+        if @watchlist
+          render json: {
+            code: 'watchlist_already_created',
+            message: 'すでにウォッチリストに登録済みです。'
+          }, status: :bad_request
+          return
+        end
 
+        Watchlist.create!(
+          watching_user: current_user,
+          watching_note: @note
+        )
+        render json: {
+          code: 'watchlist_created',
+          message: 'ウォッチリストに登録しました。'
+        }, status: :created
       end
 
       def destroy
+        unless @watchlist
+          render json: {
+            code: 'watchlist_not_created',
+            message: 'ウォッチリストに登録されていません。'
+          }, status: :bad_request
+          return
+        end
 
+        @watchlist.destroy!
+        render json: {
+          code: 'watchlist_deleted',
+          message: 'ウォッチリストから解除しました。'
+        }, status: :ok
       end
 
       private
@@ -32,6 +60,13 @@ module Api
             message: 'ノートの閲覧権限がありません。'
           }, status: :forbidden
         end
+      end
+
+      def load_watchlist
+        @watchlist = Watchlist.find_by(
+          watching_user: current_user,
+          watching_note: @note
+        )
       end
     end
   end
