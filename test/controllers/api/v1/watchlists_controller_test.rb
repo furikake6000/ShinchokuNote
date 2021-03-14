ea560ab4af7160ee8e40809fbd5951ea3223c70c
furikake6000 <ewave5000@gmail.com>
2021-03-14 13:34:21 +0900

    ウォッチリスト数に関するassertionの追加

--- test/controllers/api/v1/watchlists_controller_test.rb
+++ test/controllers/api/v1/watchlists_controller_test.rb
@@ -7,21 +7,42 @@ module Api
         include UsersHelper
 
         let(:project) { create :project }
+        let(:watcher) { create :user }
+
+        before do
+          login_for_test watcher
+        end
 
         describe 'POST /notes/{id}/watchlist' do
           subject { post api_v1_note_watchlist_path(project) }
 
-          it '201を返す' do
-            subject
+          it 'ウォッチリストが追加され201を返す' do
+            assert_difference 'Watchlist.count', 1 do
+              subject
+            end
+            assert watcher.watching_notes.include?(project)
             assert_response :created
             assert_request_schema_confirm
+          end
+
+          describe '元々ウォッチしていた場合' do
+            before { Watchlist.create!(watching_user: watcher, watching_note: project) }
+
+            it '400を返す' do
+              assert_no_difference 'Watchlist.count' do
+                subject
+              end
+              assert_response :bad_request
+            end
           end
 
           describe 'ノートが存在しない場合' do
             before { project.destroy! }
             
             it '404を返す' do
-              subject
+              assert_no_difference 'Watchlist.count' do
+                subject
+              end
               assert_response :not_found
             end
           end
@@ -30,7 +51,9 @@ module Api
             before { project.only_me_view_stance! }
 
             it '403を返す' do
-              subject
+              assert_no_difference 'Watchlist.count' do
+                subject
+              end
               assert_response :forbidden
             end
           end
@@ -39,17 +62,35 @@ module Api
         describe 'DELETE /notes/{id}/watchlist' do
           subject { delete api_v1_note_watchlist_path(project) }
 
-          it '201を返す' do
-            subject
-            assert_response :created
-            assert_request_schema_confirm
+          describe '元々登録していた場合' do
+            before { Watchlist.create!(watching_user: watcher, watching_note: project) }
+
+            it 'ウォッチリストが外れ200を返す' do
+              assert_difference 'Watchlist.count', -1 do
+                subject
+              end
+              refute watcher.watching_notes.include?(project)
+              assert_response :ok
+              assert_request_schema_confirm
+            end
+          end
+
+          describe '登録していなかった場合' do
+            it '400を返す' do
+              assert_no_difference 'Watchlist.count' do
+                subject
+              end
+              assert_response :bad_request
+            end
           end
 
           describe 'ノートが存在しない場合' do
             before { project.destroy! }
             
             it '404を返す' do
-              subject
+              assert_no_difference 'Watchlist.count' do
+                subject
+              end
               assert_response :not_found
             end
           end
@@ -58,7 +99,9 @@ module Api
             before { project.only_me_view_stance! }
 
             it '403を返す' do
-              subject
+              assert_no_difference 'Watchlist.count' do
+                subject
+              end
               assert_response :forbidden
             end
           end
