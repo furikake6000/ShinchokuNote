3d646943eb3f2a75362e47b17f2d32e17e707cf4
furikake6000 <ewave5000@gmail.com>
2021-03-14 12:51:42 +0900

    403, 404のテストの作成

--- test/controllers/api/v1/watchlists_controller_test.rb
+++ test/controllers/api/v1/watchlists_controller_test.rb
@@ -7,12 +7,6 @@ module Api
         include UsersHelper
 
         let(:project) { create :project }
-        let(:user) { project.user }
-        let(:watcher) { create :user }
-
-        before do
-          login_for_test user
-        end
 
         describe 'POST /notes/{id}/watchlist' do
           subject { post api_v1_note_watchlist_path(project) }
@@ -21,6 +15,24 @@ module Api
             subject
             assert_response :created
             assert_request_schema_confirm
+          end
+
+          describe 'ノートが存在しない場合' do
+            before { project.destroy! }
+            
+            it '404を返す' do
+              subject
+              assert_response :not_found
+            end
+          end
+
+          describe 'ノートを閲覧する権限がない場合' do
+            before { project.only_me_view_stance! }
+
+            it '403を返す' do
+              subject
+              assert_response :forbidden
+            end
           end
         end
 
@@ -31,6 +43,24 @@ module Api
             subject
             assert_response :created
             assert_request_schema_confirm
+          end
+
+          describe 'ノートが存在しない場合' do
+            before { project.destroy! }
+            
+            it '404を返す' do
+              subject
+              assert_response :not_found
+            end
+          end
+
+          describe 'ノートを閲覧する権限がない場合' do
+            before { project.only_me_view_stance! }
+
+            it '403を返す' do
+              subject
+              assert_response :forbidden
+            end
           end
         end
       end
